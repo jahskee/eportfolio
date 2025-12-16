8eb80b267d5046295c6a5833154a13be97a24c74
jaison2i <jaison2@yahoo.com>
2025-12-16 06:29:30 -0500

    Add files via upload

---
+++ v2/js/myscript.js
@@ -0,0 +1,12 @@
+function showSummary(){
+    var text = `
+    I have 15 years of experience as a software engineer,
+    I design and develop web and mobile applications. 
+    I completed the Front-End & Back-End graduate certificates at Harvard Extension School.
+    My expertise includes Angular, React, React-Native, Node, Express and GraphQL.
+    As well as experience with Docker, Kubernetes and various caching strategies & databases.`;
+
+    document.getElementById("my-summary").innerHTML = text;
+}
+
+showSummary();
